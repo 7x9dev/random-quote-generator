7394f77f5647fbef9d2e3f614cb2abcb4f1b84aa
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-11-14 23:07:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const button = document.querySelector("#btn");
 const quote = document.querySelector("#quote");
 
 const quotes = [
-   "Opportunities don't happen, you create them.",
-   "The best preparation for tomorrow is doing your best today.",
-   "It's hard to beat a person who never gives up.",
-   "Happiness is not something ready made. It comes from your own actions.",
-   "Magic is believing in yourself. If you can make that happen, you can make anything happen.",
-   "Hold the vision, trust the process.",
-   "Don't be afraid to give up the good to go for the great.",
-   "One day or day one. You decide.",
-   "Great things are done by a series of small things brought together.",
-   "The hard days are what make you stronger.",
+   `"Opportunities don't happen, you create them."`,
+   `"The best preparation for tomorrow is doing your best today."`,
+   `"It's hard to beat a person who never gives up."`,
+   `"Happiness is not something ready made. It comes from your own actions."`,
+   `"Magic is believing in yourself. If you can make that happen, you can make anything happen."`,
+   `"Hold the vision, trust the process."`,
+   `"Don't be afraid to give up the good to go for the great."`,
+   `"One day or day one. You decide."`,
+   `"Great things are done by a series of small things brought together."`,
+   `"The hard days are what make you stronger."`,
 ];
 
 button.addEventListener("click", () => {
